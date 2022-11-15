b31f27499cc821c428197fa6a015b03ddd77b37a
masashi <wf9a5m75@gmail.com>
2022-11-15 12:11:30 -0800

    stable save: implementing the today weather screen

--- android/app/src/main/java/com/example/weatherapp/ui/screens/TodayWeatherScreen.kt
+++ android/app/src/main/java/com/example/weatherapp/ui/screens/TodayWeatherScreen.kt
@@ -2,31 +2,34 @@ package com.example.weatherapp.ui.screens
 
 import android.content.Context
 import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Button
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.weatherapp.model.RetrofitHelper
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.weatherapp.R
 import com.example.weatherapp.model.Settings
 import com.example.weatherapp.model.WeatherApi
 import com.example.weatherapp.utils.NetworkUtil
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.SwipeRefreshState
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 @Preview(showBackground = true)
@@ -34,6 +37,10 @@ import kotlinx.coroutines.runBlocking
 fun TodayWeatherScreen(context: Context? = null, settings: Settings? = null, api: WeatherApi? = null) {
     val refreshState = rememberSwipeRefreshState(isRefreshing = false)
     val scrollState = rememberScrollState()
+
+    var currentWeatherIcon by remember { mutableStateOf("sunny") }
+    var lastUpdateTime by remember { mutableStateOf("") }
+
     val onRefresh : (refreshState: SwipeRefreshState) -> Unit = {
             state ->
                 if ((context != null) &&
@@ -53,31 +60,142 @@ fun TodayWeatherScreen(context: Context? = null, settings: Settings? = null, api
                         state.isRefreshing = false
 
                         val response = deferred.await()
-                        when(response.code()) {
-                            200 -> {
-                                Log.d("debug", response.body().toString())
-                            }
-
-                            else -> {
-                                // TODO:
-                            }
+                        if (response.code() != 200) {
+                            return@runBlocking
                         }
+
+                        val forecastRes = response.body()
+                        if (forecastRes == null) {
+                            return@runBlocking
+                        }
+                        currentWeatherIcon = forecastRes.forecasts[0].status
+                        lastUpdateTime = forecastRes.last_update
+                        Log.d("debug", response.body().toString())
                     }
         }
 
     }
-
+    onRefresh(refreshState)
+    
     SwipeRefresh(state = refreshState, onRefresh = {
         onRefresh(refreshState)
     }) {
 
+        Text(
+            text = "11/12/2022 14:31",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            fontSize = 32.sp,
+            color = MaterialTheme.colors.onBackground,
+            style = MaterialTheme.typography.body2
+        )
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
                 .verticalScroll(state = scrollState, enabled = true)
-                .background(color = Color.Yellow)
         ) {
-            Text("Today weather!!")
+
+            Image(
+                painter = weatherIconResource(currentWeatherIcon),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(400.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .padding(all = 32.dp)
+            )
+            Text(
+                text = "18℃",
+                fontSize = 64.sp,
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .align(Alignment.CenterHorizontally),
+                style = MaterialTheme.typography.body2
+
+            )
+            Row(
+                modifier = Modifier.fillMaxSize()
+            ) {
+
+                WeatherIcon(
+                    weather = "sunny",
+                    temperature = 18,
+                    time = "12pm",
+                    modifier = Modifier.weight(1f)
+                )
+                WeatherIcon(
+                    weather = "sunny",
+                    temperature = 18,
+                    time = "3pm",
+                    modifier = Modifier.weight(1f)
+                )
+                WeatherIcon(
+                    weather = "sunny",
+                    temperature = 18,
+                    time = "6pm",
+                    modifier = Modifier.weight(1f)
+                )
+            }
         }
+
+    }
+}
+
+@Composable
+fun WeatherIcon(
+    weather: String,
+    temperature: Int,
+    time: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .size(
+                width = 100.dp,
+                height = 130.dp
+            )
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+           modifier = Modifier
+               .fillMaxWidth()
+               .wrapContentSize(Alignment.Center),
+           text = time,
+           fontSize = 14.sp,
+           color = MaterialTheme.colors.onSecondary,
+           style = MaterialTheme.typography.body2
+        )
+
+        Image(
+            painter = weatherIconResource(weather),
+            contentDescription = "",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .align(Alignment.CenterHorizontally)
+                .padding(all = 3.dp)
+        )
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center),
+            text = "${temperature}℃",
+            fontSize = 14.sp,
+            color = MaterialTheme.colors.onSecondary,
+            style = MaterialTheme.typography.body2
+        )
+
+    }
+}
+
+@Composable
+fun weatherIconResource(weather: String): Painter {
+    return when(weather) {
+        "sunny" -> painterResource(id = R.drawable.cloud_sun)
+
+        else -> painterResource(id = R.drawable.cloud_sun)
     }
 }
