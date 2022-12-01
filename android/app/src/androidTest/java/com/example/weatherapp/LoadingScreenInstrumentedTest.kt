b7e271df7a1ccb45a986b60f50ea5d256f9ef2a1
masashi <wf9a5m75@gmail.com>
2022-11-30 17:06:36 -0800

    Fix: linter error

--- android/app/src/androidTest/java/com/example/weatherapp/LoadingScreenInstrumentedTest.kt
+++ android/app/src/androidTest/java/com/example/weatherapp/LoadingScreenInstrumentedTest.kt
@@ -1,19 +1,15 @@
 package com.example.weatherapp
 
-import android.content.Context
 import androidx.activity.ComponentActivity
 import androidx.annotation.StringRes
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import com.example.weatherapp.ui.screens.LoadingScreen
 import org.junit.Rule
 import org.junit.Test
 
 class LoadingScreenInstrumentedTest {
-
 
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
@@ -30,6 +26,5 @@ class LoadingScreenInstrumentedTest {
             onNodeWithText(getString(R.string.loading_message))
                 .assertIsDisplayed()
         }
-
     }
 }
